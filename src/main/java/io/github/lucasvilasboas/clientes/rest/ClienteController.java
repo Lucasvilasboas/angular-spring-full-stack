6e62d9d9184b451a936392982bbbc5511d9ab40e
Lucas Vilas Boas <vilasboaslucas90@gmail.com>
2022-06-17 21:20:46 -0300

    Implementando funcionalidade deletar cliente

--- src/main/java/io/github/lucasvilasboas/clientes/rest/ClienteController.java
+++ src/main/java/io/github/lucasvilasboas/clientes/rest/ClienteController.java
@@ -2,11 +2,15 @@ package io.github.lucasvilasboas.clientes.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import io.github.lucasvilasboas.clientes.model.entity.Cliente;
 import io.github.lucasvilasboas.clientes.model.repository.ClienteRepository;
@@ -29,4 +33,21 @@ public class ClienteController {
 		return repository.save(cliente);
 	}
 	
-}
+	@GetMapping("{id}")
+	public Cliente acharPorId(@PathVariable Integer id) {
+		return repository.findById(id).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+	
+	@DeleteMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar (@PathVariable Integer id ) {
+		
+		 repository.findById(id).map(cliente -> {
+			 repository.delete(cliente);
+			 return Void.TYPE;
+		 })
+		 .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+		 }
+	}
+	
+
