d4550382641c50b43297af773ab34acbd4d691ac
Lucas Vilas Boas <vilasboaslucas90@gmail.com>
2022-06-17 23:46:22 -0300

    atualizando/validando cliente

--- src/main/java/io/github/lucasvilasboas/clientes/model/entity/Cliente.java
+++ src/main/java/io/github/lucasvilasboas/clientes/model/entity/Cliente.java
@@ -8,6 +8,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.br.CPF;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -28,12 +32,15 @@ public class Cliente {
 	private Integer id;
 	
 	@Column(nullable = false, length = 150)
+	@NotEmpty(message = "{campo.nome.obrigatorio}")
 	private String nome;
 	
 	@Column(nullable = false, length = 11)
+	@NotNull(message = "{campo.cpf.obrigatorio}")
+	@CPF(message = "{campo.cpf.invalido}")
 	private String cpf;
 	
-	@Column (name = "data_cadastro")
+	@Column (name = "data_cadastro", updatable = false)
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataCadastro;
 		
@@ -41,4 +48,6 @@ public class Cliente {
 	public void prePersist() {
 		setDataCadastro(LocalDate.now());
 	}
+	
+	
 }
